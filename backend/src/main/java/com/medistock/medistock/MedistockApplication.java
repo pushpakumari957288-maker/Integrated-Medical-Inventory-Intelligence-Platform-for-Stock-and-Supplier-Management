e4d97769e7672fcce461c44c7608403945d62e80
PRIYANKA-T-M <priyanka0503007@gmail.com>
2026-09-01 23:30:19 +0530

    implement milestone 1 backend authentication

--- backend/src/main/java/com/medistock/medistock/MedistockApplication.java
+++ backend/src/main/java/com/medistock/medistock/MedistockApplication.java
@@ -2,9 +2,8 @@ package com.medistock.medistock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 
 public class MedistockApplication {
 
