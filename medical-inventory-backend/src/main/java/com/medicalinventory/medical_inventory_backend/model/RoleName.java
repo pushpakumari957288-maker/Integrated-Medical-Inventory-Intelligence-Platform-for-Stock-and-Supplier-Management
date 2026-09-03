75531559950daa2f70766b799590a75a78c93cd0
shaikhrizwan988010-lang <shaikhrizwan988010@gmail.com>
2026-09-03 18:01:49 +0530

    Revert "Initial commit: Medical Inventory Backend - Spring Boot application w…"

--- medical-inventory-backend/src/main/java/com/medicalinventory/medical_inventory_backend/model/RoleName.java
+++
@@ -1,3 +0,0 @@
-package com.medicalinventory.medical_inventory_backend.model;
-
-public enum RoleName { ADMIN, PHARMACIST, STAFF }
--- medical-inventory-backend/src/main/java/com/medicalinventory/medical_inventory_backend/repository/RoleRepository.java
+++
@@ -1,3 +0,0 @@
-package com.medicalinventory.medical_inventory_backend.repository;
-import com.medicalinventory.medical_inventory_backend.model.*; import org.springframework.data.jpa.repository.JpaRepository; import java.util.Optional;
-public interface RoleRepository extends JpaRepository<Role,Long> { Optional<Role> findByName(RoleName name); }
